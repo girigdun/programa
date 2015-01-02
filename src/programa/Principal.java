4b3482be9bbb1aa721305394f88b4e8038aea748
Felipe <Felipe@Felipe-PC>
2015-01-02 14:53:44 -0200

    Foi adicionado uma classe ClienteBD e substituido as variaveis por varaiveis com get e set

--- src/programa/Principal.java
+++ src/programa/Principal.java
@@ -7,6 +7,7 @@ package programa;
 
 import java.sql.SQLException;
 import java.util.Scanner;
+import sql.ClienteBD;
 import sql.comando;
 import sql.conecta;
 
@@ -16,33 +17,31 @@ import sql.conecta;
  */
 public class Principal {
 
-    public static String nome;                                                  //objeto do tipo String chamado : nome "campo nome do BD"
-    public static int id;                                                     //objeto do tipo int chamado : b_id "busca do ID"
-
-    public static void main(String[] args) throws SQLException, Exception {     //metodo principal
-        Scanner sc = new Scanner(System.in);                                    // chamando Scanner
-        conecta.conexao();                                                      // chamando metodo conexao da classe conecta do pacote sql
-        
-        System.out.println("Digite um nome de cliente");                        //mensagem para ação
-        nome = sc.next();                                                       //captura do teclado o nome para ser inserido no bd
-        comando.insert(nome);                                                       // chamando metodo insert da classe comando do pacote sql
+    public static void main(String[] args) throws SQLException, Exception {     
+        Scanner sc = new Scanner(System.in);                                    
+        conecta.conexao();                                                      
+        ClienteBD cliente = new ClienteBD();
+    
+        System.out.println("Digite um nome de cliente");                        
+        cliente.setNome(sc.next());                                             
+        comando.insert(cliente.getNome());                                      
         
         System.out.println("");
         
-        System.out.println("Digite um ID para busca:");                         //mensagem para ação
-        id = sc.nextInt();                                                    //captura do teclado o id para busca no bd
-        comando.select(id);                                                       // chamando metodo select da classe comando do pacote sql
+        System.out.println("Digite um ID para busca:");                         
+        cliente.setId(sc.nextInt());                                            
+        comando.select(cliente.getId());                                        
         
         System.out.println("Digite o ID do cadastro para ser auterado");
-        id= sc.nextInt();
+        cliente.setId(sc.nextInt());
         System.out.println("Digite o novo valor do campo Nome:");
-        nome = sc.next();
-        comando.update(nome, id);
+        cliente.setNome(sc.next());
+        comando.update(cliente.getNome(), cliente.getId());
         
         System.out.println("Digite o ID do nome para deletar:");
-        id= sc.nextInt();
-        comando.delete(id);
+        cliente.setId(sc.nextInt());
+        comando.delete(cliente.getId());
         
-        conecta.desconecta();                                                   // chama o metodo desconecta da classe concta do pacote sql para finalizar a conexao
+        conecta.desconecta();                                                 
     }
 }
